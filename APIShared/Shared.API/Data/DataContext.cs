a8ef12a41aa4edaaccb2f9c47417263e6ccfedfb
Alex Estrada. <ingenieroalexestrada@gmail.com>
2024-04-27 10:20:22 -0500

    Primer commint

--- APIShared/Shared.API/Data/DataContext.cs
+++ APIShared/Shared.API/Data/DataContext.cs
@@ -5,11 +5,11 @@ namespace Shared.API.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        public DataContext(DbContextOptions<DataContext> company) : base(company)
         {
         }
 
-        public DbSet<User> Users { get; set; }
+        public DbSet<Company> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
---
+++ APIShared/Shared.Commond/Entities/company.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Common.Entities
+{
+    public class Company
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [MaxLength(100)]
+        public string Address { get; set; } = null!;
+
+        [Required]
+        [MaxLength(20)]
+        [MinLength(20)]
+        public string Phone { get; set; } = null!;
+
+        [Required]
+        [MaxLength(100)]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [MaxLength(20)]
+        [MinLength(4)]
+        public string Manager { get; set; } = null!;
+    }
+}
