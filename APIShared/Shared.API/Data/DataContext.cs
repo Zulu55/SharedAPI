71da6f28012fdbf51039c749b77bcf472847f22d
Juan Zuluaga <juan.zuluaga@cscgfm.com>
2024-04-27 10:55:06 -0500

    Fix order in data context

--- APIShared/Shared.API/Data/DataContext.cs
+++ APIShared/Shared.API/Data/DataContext.cs
@@ -13,8 +13,8 @@ namespace Shared.API.Data
         public DbSet<Company> Companies { get; set; }
         public DbSet<Employee> Employees { get; set; }
         public DbSet<Product> Products { get; set; }
-        public DbSet<User> Users { get; set; }
         public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -22,8 +22,8 @@ namespace Shared.API.Data
             modelBuilder.Entity<Company>().HasIndex(x => x.Name).IsUnique();
             modelBuilder.Entity<Client>().HasIndex(x => x.Document).IsUnique();
             modelBuilder.Entity<Product>().HasIndex(x => x.Name).IsUnique();
-            modelBuilder.Entity<User>().HasIndex(x => x.Email).IsUnique();
             modelBuilder.Entity<Supplier>().HasIndex(x => x.Name).IsUnique();
+            modelBuilder.Entity<User>().HasIndex(x => x.Email).IsUnique();
         }
     }
 }
