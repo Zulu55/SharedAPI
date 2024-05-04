6bb97d22cb466f99e93f600f2bba0e0caf9f48b9
DESKTOP-H5BF84E\Usuario <aecheverrya@misena.edu.co>
2024-05-04 09:56:19 -0500

    Activities of second class ready

---
+++ APIShared/Shared.API/Controllers/ProductsController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shared.API.Data;
+using Shared.Common.Entities;
+
+
+
+namespace Shared.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ProductsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _context.Products.ToListAsync());
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return Ok(product);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> PutAsync(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest("The ids doesn't match.");
+            }
+
+            var currentProduct = await _context.Products.FindAsync(id);
+            if (currentProduct == null)
+            {
+                return NotFound();
+            }
+
+            currentProduct.Name = product.Name;
+            currentProduct.Description = product.Description;
+            currentProduct.Price = product.Price;
+            currentProduct.Discount = product.Discount;
+
+            _context.Products.Update(currentProduct);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+    }
+}
---
+++ APIShared/Shared.API/Data/SeedDb.cs
@@ -0,0 +1,31 @@
+﻿using Shared.Common.Entities;
+
+namespace Shared.API.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+
+        public SeedDb(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+            await CheckProductsAsync();
+        }
+
+        private async Task CheckProductsAsync()
+        {
+            if (!_context.Products.Any()) {
+                _context.Products.Add(new Product { Name = "jacket", Description = "jacket of leather", Price = 1000, Discount = 10 });
+                _context.Products.Add(new Product { Name = "shoes", Description = "multiple sizes", Price = 1000, Discount = 12 });
+                _context.Products.Add(new Product { Name = "skirt", Description = "multiple sizes and colors", Price = 80, Discount = 9 });
+                _context.Products.Add(new Product { Name = "cap", Description = "multiple styles", Price = 80, Discount = 1 });
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
